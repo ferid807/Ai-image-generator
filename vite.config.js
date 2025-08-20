66a9e69ae2c34eab82d716ba7375c6b81a672b75
Farid Karimli <feridkerimli807@gmail.com>
2025-08-20 21:54:10 +0400

    Fix themes, add neon theme, update background, and fix explore images

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import './index.css';
-
 // For GitHub Pages project site at https://<user>.github.io/Ai-image-generator/
 export default defineConfig({
   base: '/Ai-image-generator/',
